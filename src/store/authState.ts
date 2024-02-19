cfccc123c242305320ff0f7f1035b3a20ae52957
roshan1697 <roshanpoojary16@gmail.com>
2024-02-19 17:58:29 +0530

    fix:backend typescript runtime

--- src/store/authState.ts
+++ src/store/authState.ts
@@ -1,10 +1,10 @@
 import { atom } from "recoil";
-interface TodoType {
-    _id:string;
-    title:string;
-    description:string;
-    done:boolean;
-  }
+// interface TodoType {
+//     _id:string;
+//     title:string;
+//     description:string;
+//     done:boolean;
+//   }
 export const authState = atom({
     key:'authState',
     default:{
